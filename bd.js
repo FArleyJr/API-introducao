bde010c3ef6e6dc5edd3873b42cb25879906520a
FArleyJr <133126769+FArleyJr@users.noreply.github.com>
2023-08-11 13:59:16 +0000

    foi

--- bd.js
+++ bd.js
@@ -9,10 +9,31 @@ async function connect() {
   return pool.connect();
 }
 
+//bd.js
+async function insertUsuario(data) {
+  const client = await connect();
+  const query = "INSERT INTO usuario (nome,senha,email) VALUES ($1,$2,$3) ";
+  const usuario = [data.nome, data.senha, data.email];
+  await client.query(query, usuario);
+}
+
 async function selectUsuarios() {
   const client = await connect();
   const res = await client.query("SELECT * FROM usuario");
   return res.rows;
 }
 
-export { selectUsuarios };
+//bd.js
+async function selectUsuario(id) {
+  const client = await connect();
+  const query = "SELECT * FROM usuario WHERE id = $1";
+  const usuario = [id];
+  const res = await client.query(query, usuario);
+  return res.rows;
+}
+
+
+
+
+
+export { selectUsuarios, selectUsuario, insertUsuario };
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from "express";      // Requisição do pacote do express
 
-//index.js
 import dotenv from "dotenv";
-import { selectUsuarios } from "./bd.js";
-
+import { selectUsuarios, selectUsuario, insertUsuario } from "./bd.js";
 
 dotenv.config();
 
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
+app.use(express.json());
 
 app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   res.json({
@@ -21,6 +20,8 @@ app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
 });
 
+
+
 app.get("/usuarios", async (req, res) => {
   console.log("Rota GET/usuarios solicitada");
   try {
@@ -29,4 +30,27 @@ app.get("/usuarios", async (req, res) => {
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
+  //11/08
+  app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+  //index.js
+
+
+app.post("/usuario", async (req, res) => {
+  console.log("Rota POST /usuario solicitada");
+  try {
+    await insertUsuario(req.body);
+    res.status(201).json({ message: "Usuário inserido com sucesso!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
 });
